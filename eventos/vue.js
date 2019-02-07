ed156096299baefb031448851e4dc903d412f67e
JosAAV <jose_antonio_alvino21@outlook.es>
2019-02-07 11:06:36 -0500

    empezando a conocer los eventos con vueJs

---
+++ eventos/vue.js
@@ -0,0 +1,16 @@
+new Vue({
+    el:'#eventos',
+    data:{
+        contador:0
+    },
+    methods:{
+        sumarUno:function()
+        {
+            this.contador +=1;
+        },
+        restarUno:function()
+        {
+            this.contador -=1;
+        },
+    }
+})
