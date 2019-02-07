90f7153f988952a3a553035fd42d3da6f584abf6
JosAAV <jose_antonio_alvino21@outlook.es>
2019-02-07 14:29:45 -0500

    terminando de conocer los eventos con VueJS

--- eventos/vue.js
+++ eventos/vue.js
@@ -1,7 +1,10 @@
 new Vue({
     el:'#eventos',
     data:{
-        contador:0
+        contador:0,
+        x:0,
+        y:0,
+        clicks:0
     },
     methods:{
         sumarUno:function()
@@ -12,5 +15,21 @@ new Vue({
         {
             this.contador -=1;
         },
+        alerta:function(mensaje){
+            alert(mensaje);
+        },
+        mostrarUbicacion(evento){
+            //Estamos obteniendo el envento de mousemove
+            // y estamos pidiendo que nos brinde la posicion en X y Y
+            this.x = evento.clientX;
+            this.y = evento.clientY;
+        },
+        sumar:function(cantidad){
+            this.contador+=cantidad;
+        },
+        clickButton:function(){
+            this.clicks+=1;
+        }
+        
     }
 })
