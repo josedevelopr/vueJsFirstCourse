fb9d099f379b61004f1bf3636f4303a7c56df670
JosAAV <jose_antonio_alvino21@outlook.es>
2019-02-07 08:52:06 -0500

    Empezando a conocer las directivas

---
+++ directivas/vue.js
@@ -0,0 +1,9 @@
+var app = new Vue(
+    {
+        el : '#vm',
+        data:{
+          imagen:'http://lorempixel.com/300/200',
+          texto : 'Bienvenidos al curso de vue.js 2!'
+        },           
+    }
+)
