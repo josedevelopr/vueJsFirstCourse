d72e78cb10f46d5ba87534c5faddde55324dcade
JosAAV <jose_antonio_alvino21@outlook.es>
2019-02-07 10:54:33 -0500

    terminando de conocer las directivas

--- directivas/vue.js
+++ directivas/vue.js
@@ -2,8 +2,34 @@ var app = new Vue(
     {
         el : '#vm',
         data:{
-          imagen:'http://lorempixel.com/300/200',
-          texto : 'Bienvenidos al curso de vue.js 2!'
-        },           
+            imagen:'http://lorempixel.com/300/200',
+            texto : 'Bienvenidos al curso de vue.js 2!',
+            html : '<em>Bienvenidos al curso de vue.js 2!</em>',
+            mostrar:false ,
+            parrafo:4,
+            paises:[
+                {
+                    nombre:'Mexico',
+                    idioma:'español'
+                },
+                {
+                    nombre:'España',
+                    idioma:'español'
+                },
+                {
+                    nombre:'Colombia',
+                    idioma:'español'
+                }
+            ],
+            empleado:{
+                nombre:'Carlos Castro',
+                edad:'30 años',
+                nacionalidad:'Mexicano',
+                profesion:'Programador',
+                empresa:'IBM',
+                telefono:'978654312'
+            }
+
+        },    
     }
 )
